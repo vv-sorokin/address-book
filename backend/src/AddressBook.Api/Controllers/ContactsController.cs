47fb5361fddfbe3cdf90b494ea7ad89f9487950f
Vladimir Sorokin <vladimirsorokin.vl@gmail.com>
2026-02-24 18:42:48 +0100

    feat: add contacts endpoints

--- backend/src/AddressBook.Api/Controllers/ContactsController.cs
+++ backend/src/AddressBook.Api/Controllers/ContactsController.cs
@@ -1,15 +1,40 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AddressBook.Application.Contacts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AddressBook.Api.Controllers
+namespace AddressBook.Api.Controllers;
+
+[Route("contacts")]
+[ApiController]
+[Authorize]
+public class ContactsController : ControllerBase
 {
-    [Route("contacts")]
-    [ApiController]
-    [Authorize]
-    public class ContactsController : ControllerBase
+    private readonly IContactsService _contacts;
+
+    public ContactsController(IContactsService contacts)
     {
-
-
+        _contacts = contacts;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetMy(CancellationToken ct)
+        => Ok(await _contacts.GetMyAsync(ct));
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateContactRequest request, CancellationToken ct)
+        => Ok(await _contacts.CreateAsync(request, ct));
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id,  UpdateContactRequest request, CancellationToken ct)
+        => Ok(await _contacts.UpdateAsync(id, request, ct));
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+    {
+        await _contacts.DeleteAsync(id, ct);
+        return NoContent();
+    }
+
 }
